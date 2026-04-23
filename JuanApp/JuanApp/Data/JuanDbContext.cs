6e73f68195ef982f7b67c3dd8798a4ba22ef0d67
fatimalikova <fatimakhm@code.edu.az>
2026-04-23 23:25:41 +0400

    Start backend app

---
+++ JuanApp/JuanApp/Data/JuanDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace JuanApp.Data
+{
+    public class JuanDbContext(DbContextOptions<JuanDbContext> options) : DbContext(options)
+    {
+        
+        // Define your DbSets here, for example:
+        // public DbSet<Product> Products { get; set; }
+    }
+}
